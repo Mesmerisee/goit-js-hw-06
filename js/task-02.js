99f11e8ab8a2d01906392c66800fc82b9278d464
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-05 23:24:27 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients")
+
+ingredients.map(el => 
+{const itemEl = document.createElement("li");
+itemEl.classList.add("item");
+itemEl.innerHTML = el;
+ingredientsEl.append(itemEl);
+console.log(itemEl);
+})
