d8f478e5204df2e78fa784001de27f5a4885c53f
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-05 18:52:52 +0300

    task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,9 @@
-const itemEls = document.querySelector("#categories");
-console.log(itemEls);
+const itemEls = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemEls.length}`);
 
-//Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-//Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-//Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+itemEls.forEach((el) => {
+  console.log(
+    `Category: ${el.firstElementChild.textContent}
+Elements: ${el.lastElementChild.children.length}`
+  );
+});
