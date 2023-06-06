02372bfd2e49b613404adbb509d970c904d8d2e8
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-06 23:09:15 +0300

    task 9 partially done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const widgetEl = document.querySelector(".widget");
+const textEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+btnEl.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  document.body.style.backgroungColor = color;
+  textEl.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
