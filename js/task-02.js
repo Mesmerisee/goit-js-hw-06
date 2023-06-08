1339bade7e7e34195749ccb3760421458dc48e89
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-08 22:34:30 +0300

    task 2 corrected

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
-
-const ingredientsEl = document.querySelector("#ingredients")
-
-ingredients.map(el => 
-{const itemEl = document.createElement("li");
-itemEl.classList.add("item");
-itemEl.innerHTML = el;
-ingredientsEl.append(itemEl);
-console.log(itemEl);
-})
+const ingredientsEl = document.querySelector("#ingredients");
+const addIngredients = ingredients.map((el) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = el;
+  return itemEl;
+});
+ingredientsEl.append(...addIngredients);
+console.log(ingredientsEl);
