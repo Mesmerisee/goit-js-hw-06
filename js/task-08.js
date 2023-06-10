bfd81e6d0405f6cb54991c21668b724ba00e3588
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-10 20:05:17 +0300

    Task 8 corrected

--- js/task-08.js
+++ js/task-08.js
@@ -5,17 +5,19 @@ loginEl.addEventListener("submit", handleSubmit);
 const loginForm = {};
 
 function handleSubmit(e) {
+    e.preventDefault();
   const userEmail = e.currentTarget.elements.email;
   const userPassword = e.currentTarget.elements.password;
 
   if (userEmail.value === "" || userPassword.value === "") {
     alert("All fields must be filled!");
-    e.preventDefault();
+   
   } else {
     loginForm.userEmail = userEmail.value;
     loginForm.userPassword = userPassword.value;
     alert("Login successful!");
+    console.log(loginForm);
     e.currentTarget.reset();
   }
-  console.log(loginForm);
+  
 }
